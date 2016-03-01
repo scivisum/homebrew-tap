861af74dca51e1128074bbbe818bbe70cd907ee4
Charles Daffern <charles.daffern@scivisum.co.uk>
2016-03-01 16:30:26 +0000

    Changed my mind about the path

--- vncsnapshot-png.rb
+++ vncsnapshot-png.rb
@@ -2,7 +2,7 @@ class VncsnapshotPng < Formula
   desc "Utility for taking snapshots of a vnc server in PNG format"
   homepage "https://github.com/ScoreUnder/vncsnapshot-png"
   head "https://github.com/ScoreUnder/vncsnapshot-png.git"
-  url "http://mac-repo.scivisum.co.uk/sources/vncsnapshot-png-1.3.tar.xz"
+  url "http://mac-repo.scivisum.co.uk/sources/vncsnapshot-png/vncsnapshot-png-1.3.tar.xz"
   sha256 "bad88eb750c9796a3685e7d29b3a6410556e93795defd760666c7dd11525aa72"
 
   depends_on "libpng"
