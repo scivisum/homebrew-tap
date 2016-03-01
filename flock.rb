ba26532bc2faa55bf3b3ebff88f123bb85a5eede
Charles Daffern <charles.daffern@scivisum.co.uk>
2016-03-01 17:17:23 +0000

    Bottle flock locally

--- flock.rb
+++ flock.rb
@@ -6,6 +6,12 @@ class Flock < Formula
   url "http://mac-repo.scivisum.co.uk/sources/flock/flock-#{version}.tar.xz"
   sha256 '3233658199683c807c21b0ef0fc32246e420f2a6e48f7044d2ccb763ff320c70'
 
+  bottle do
+    root_url "http://mac-repo.scivisum.co.uk/binaries/flock"
+    cellar :any_skip_relocation
+    sha256 "e5d83dad3618e5f39bb88004f578b5ecdad9d926aac2a5f9b36e7d5bd9b3e07f" => :yosemite
+  end
+
   def install
     system './configure', '--disable-debug',
                           '--disable-dependency-tracking',
