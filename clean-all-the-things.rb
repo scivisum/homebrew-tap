5e5da620243d86b58d033d4804425997673ce4b9
Charles Daffern <charles.daffern@scivisum.co.uk>
2016-03-14 13:54:04 +0000

    The version isn't stable per se
    And salt doesn't seem to like stable versions either.

--- clean-all-the-things.rb
+++ clean-all-the-things.rb
@@ -1,11 +1,8 @@
 class CleanAllTheThings < Formula
   desc "SciVisum server temporary file cleanup tool"
   head "git@git:sysadmin", :using => :git
-
-  stable do
-    url "http://mac-repo.scivisum.co.uk/sources/clean-all-the-things/clean-all-the-things-1.0.302.tar.xz"
-    sha256 "149e231726882dc3764c775257665d5185139609e280783d0d5ea2ed5cb112f3"
-  end
+  url "http://mac-repo.scivisum.co.uk/sources/clean-all-the-things/clean-all-the-things-1.0.302.tar.xz"
+  sha256 "149e231726882dc3764c775257665d5185139609e280783d0d5ea2ed5cb112f3"
 
   def install
     bin.install "scripts/cleanAllTheThings/bin/cleanAllTheThings"
