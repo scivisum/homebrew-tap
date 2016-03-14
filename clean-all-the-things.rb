51e05bc38dd0c9c4ba30c7a2306257866010a938
Charles Daffern <charles.daffern@scivisum.co.uk>
2016-03-14 13:01:07 +0000

    On second thoughts, the bottle is just going to seriously complicate things

--- clean-all-the-things.rb
+++ clean-all-the-things.rb
@@ -7,12 +7,6 @@ class CleanAllTheThings < Formula
     sha256 "149e231726882dc3764c775257665d5185139609e280783d0d5ea2ed5cb112f3"
   end
 
-  bottle do
-    root_url "http://mac-repo.scivisum.co.uk/binaries/clean-all-the-things"
-    cellar :any_skip_relocation
-    sha256 "0697c28f9c2d7a5010fff937d5e7bc3b6b53ef14a1fa001654214994ac1d5c4b" => :yosemite
-  end
-
   def install
     bin.install "scripts/cleanAllTheThings/bin/cleanAllTheThings"
   end
