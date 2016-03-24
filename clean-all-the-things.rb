10e2655f04fbecdd502586860ce97724e06af512
Charles Daffern <charles.daffern@scivisum.co.uk>
2016-03-24 16:15:26 +0000

    Update CATT

--- clean-all-the-things.rb
+++ clean-all-the-things.rb
@@ -1,8 +1,8 @@
 class CleanAllTheThings < Formula
   desc "SciVisum server temporary file cleanup tool"
   head "git@git:sysadmin", :using => :git
-  url "http://mac-repo.scivisum.co.uk/sources/clean-all-the-things/clean-all-the-things-1.0.302.tar.xz"
-  sha256 "149e231726882dc3764c775257665d5185139609e280783d0d5ea2ed5cb112f3"
+  url "http://mac-repo.scivisum.co.uk/sources/clean-all-the-things/clean-all-the-things-1.0.311.tar.xz"
+  sha256 "4d19308353b51b6dc49d5bde5fb0df1e229d31d5a53025bae08a50d181f5d2b5"
 
   def install
     bin.install "scripts/cleanAllTheThings/bin/cleanAllTheThings"
