21d0b700d130e3a1fd35872e2751b503971227ed
Charles Daffern <charles.daffern@scivisum.co.uk>
2016-09-21 13:40:56 +0100

    Add URL to tsproxy formula

--- Formula/tsproxy.rb
+++ Formula/tsproxy.rb
@@ -1,5 +1,7 @@
 class Tsproxy < Formula
     desc "Traffic shaping proxy"
+    url "http://mac-repo.scivisum.co.uk/sources/tsproxy/tsproxy-0.0.26.tar.xz"
+    sha256 "2c5e05e5bb03ee75b2859c284afa33c755336f15cc307179744b105b45a8fa68"
     head "https://github.com/WPO-Foundation/tsproxy.git", :using => :git
 
     def install
