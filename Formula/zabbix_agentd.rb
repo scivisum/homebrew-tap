ccf3c4c55831a7ce5610e99d4a7554c7d0e557b1
nsysadmin <56231393+nsysadmin@users.noreply.github.com>
2019-10-08 09:24:11 +0100

    Add formula for zabbix_agentd

---
+++ Formula/zabbix_agentd.rb
@@ -0,0 +1,12 @@
+class ZabbixAgentd < Formula
+    desc "zabbix agentd for osx"
+    homepage "https://www.zabbix.org/"
+    url "http://mac-repo.scivisum.co.uk/sources/zabbix_agentd/zabbix-3.4.7.tar.gz"
+    sha256 "ae0f5c7da3886aa3184a1c39ba455e801cdc4356ba16bf68339aee0947366289"
+
+    def install
+        system "./configure", "--enable-agent", "--prefix=/usr/local/Cellar/zabbix_agentd"
+        system "make"
+	    sbin.install "src/zabbix_agent/zabbix_agentd"
+    end 
+end 
