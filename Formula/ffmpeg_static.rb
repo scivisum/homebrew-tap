bf6e1ee65f5e596e4176b60d7020fc9960ce2cf8
Nikita Soldatov <nikita.soldatov@thinktribe.com>
2021-11-10 12:56:09 +0000

    Install ffprobe alongside ffmpeg

--- Formula/ffmpeg_static.rb
+++ Formula/ffmpeg_static.rb
@@ -1,8 +1,10 @@
 class FfmpegStatic < Formula
     desc "ffmpeg static build"
-    url "http://mac-repo.scivisum.co.uk/binaries/ffmpeg_static/ffmpeg-4.4.1.zip"
-    sha256 "d7f3ea44b6086834fac4198f2f0a82f93ac9fba587d94f6b086b16ed228fcdb3"
+    url "http://mac-repo.scivisum.co.uk/binaries/ffmpeg_static/ffmpeg_static.tar.gz"
+    sha256 "5a7484449cc4d813fd0c5535eab6d3addc86884c3b2ccfcf2e71547655721a0e"
+    version "4.4.1"
     def install
         bin.install "ffmpeg"
+	bin.install "ffprobe"
     end
 end
