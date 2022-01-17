4d692e1671358a120168ba79f97d0889c6b0e04a
Sophie Daffern <sophie.daffern@thinktribe.com>
2022-01-17 16:57:09 +0000

    Add libiconv dependency to zabbix_agentd

--- Formula/zabbix_agentd.rb
+++ Formula/zabbix_agentd.rb
@@ -4,6 +4,8 @@ class ZabbixAgentd < Formula
     url "http://mac-repo.scivisum.co.uk/sources/zabbix_agentd/zabbix-3.4.7.tar.gz"
     sha256 "ae0f5c7da3886aa3184a1c39ba455e801cdc4356ba16bf68339aee0947366289"
 
+    depends_on "libiconv"
+
     def install
         system "./configure", "--enable-agent", "--prefix=/usr/local/Cellar/zabbix_agentd"
         system "make"
