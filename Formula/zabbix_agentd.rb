bf6d48c84c300d904be36ca79f68c35142faebf6
Sophie Daffern <sophie.daffern@thinktribe.com>
2023-04-24 14:47:04 +0100

    Make zabbix_agentd depend on pcre

--- Formula/zabbix_agentd.rb
+++ Formula/zabbix_agentd.rb
@@ -3,12 +3,15 @@ class ZabbixAgentd < Formula
     homepage "https://www.zabbix.org/"
     url "http://mac-repo.scivisum.co.uk/sources/zabbix_agentd/zabbix-3.4.7.tar.gz"
     sha256 "ae0f5c7da3886aa3184a1c39ba455e801cdc4356ba16bf68339aee0947366289"
+    revision 1
 
     depends_on "libiconv"
+    depends_on "pcre"
 
     def install
         iconv_prefix = Formula["libiconv"].opt_prefix
-        system *%W(./configure --enable-agent --prefix=#{prefix} --with-iconv=#{iconv_prefix})
+        pcre_prefix = Formula["pcre"].opt_prefix
+        system *%W(./configure --enable-agent --prefix=#{prefix} --with-iconv=#{iconv_prefix} --with-pcre=#{pcre_prefix})
         system "make"
         sbin.install "src/zabbix_agent/zabbix_agentd"
     end
