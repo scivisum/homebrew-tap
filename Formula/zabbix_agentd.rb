72e6ed6c6cdcf2db6beb2b405cf5f5d03b04b48c
Sophie Daffern <sophie.daffern@thinktribe.com>
2023-04-24 14:49:19 +0100

    Correct argument to configure; use https

--- Formula/zabbix_agentd.rb
+++ Formula/zabbix_agentd.rb
@@ -1,7 +1,7 @@
 class ZabbixAgentd < Formula
     desc "zabbix agentd for osx"
     homepage "https://www.zabbix.org/"
-    url "http://mac-repo.scivisum.co.uk/sources/zabbix_agentd/zabbix-3.4.7.tar.gz"
+    url "https://mac-repo.scivisum.co.uk/sources/zabbix_agentd/zabbix-3.4.7.tar.gz"
     sha256 "ae0f5c7da3886aa3184a1c39ba455e801cdc4356ba16bf68339aee0947366289"
     revision 1
 
@@ -11,7 +11,7 @@ class ZabbixAgentd < Formula
     def install
         iconv_prefix = Formula["libiconv"].opt_prefix
         pcre_prefix = Formula["pcre"].opt_prefix
-        system *%W(./configure --enable-agent --prefix=#{prefix} --with-iconv=#{iconv_prefix} --with-pcre=#{pcre_prefix})
+        system *%W(./configure --enable-agent --prefix=#{prefix} --with-iconv=#{iconv_prefix} --with-libpcre=#{pcre_prefix})
         system "make"
         sbin.install "src/zabbix_agent/zabbix_agentd"
     end
