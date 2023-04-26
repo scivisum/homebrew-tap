b744ca3dd87a7160dccdbb02ed0026288fc618a3
Sophie Daffern <sophie.daffern@thinktribe.com>
2023-04-26 15:00:21 +0100

    Build collectd with python3.11 support

--- Formula/collectd.rb
+++ Formula/collectd.rb
@@ -2,7 +2,7 @@ class Collectd < Formula
   desc "Statistics collection and monitoring daemon"
   homepage "https://collectd.org/"
   license "MIT"
-  revision 5
+  revision 6
 
   stable do
     url "https://mac-repo.scivisum.co.uk/sources/collectd/collectd-5.12.0.tar.bz2"
@@ -13,17 +13,6 @@ class Collectd < Formula
       url "https://mac-repo.scivisum.co.uk/sources/collectd/configure-big_sur.diff"
       sha256 "35acd6aebc19843f1a2b3a63e880baceb0f5278ab1ace661e57a502d9d78c93c"
     end
-  end
-
-  bottle do
-    sha256 arm64_ventura:  "17ef602f303744b198fa038d7b7aea63147e4da61d756614d7765aed9b8c1e16"
-    sha256 arm64_monterey: "dcc3153be48b21d9ed70ad1f73a4ef3e85f4d0a05df1a66f740837b98812f4f4"
-    sha256 arm64_big_sur:  "9b6ef1f192b583587b38842eeb4487472cc120ab113884af1badd94d7f81d718"
-    sha256 ventura:        "b43512984ffee809d62ae814d028b6eb838675da3f2a9d2c0209bec4d7718c1f"
-    sha256 monterey:       "fa6fb93745bfa378667bf760f47dfbdca543979b51e354921639a3e279cb3139"
-    sha256 big_sur:        "722677e0ee31b375470ec75e6bfe40c67ea6e57549eb918e40a0a5a8861984db"
-    sha256 catalina:       "52ee6f9dceeda3c68fbb3f12c585d5725dfdfd7815a6bfcf866ac72d00d884e8"
-    sha256 x86_64_linux:   "11b0e03c4fbf06b9f467d7bb98427a8e0e0a9efc70c376a7c766afce668f4909"
   end
 
   head do
@@ -38,6 +27,7 @@ class Collectd < Formula
   depends_on "libtool"
   depends_on "net-snmp"
   depends_on "riemann-client"
+  depends_on "python@3.11"
 
   uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
@@ -48,9 +38,11 @@ class Collectd < Formula
       --localstatedir=#{var}
       --disable-java
       --enable-write_riemann
+      --enable-python
     ]
     args << "--with-perl-bindings=PREFIX=#{prefix} INSTALLSITEMAN3DIR=#{man3}" if OS.linux?
 
+    ENV["PYTHON_CONFIG"] = Formula["python@3.11"].opt_bin/"python3-config"
     system "./build.sh" if build.head?
     system "./configure", *args
     system "make", "install"
